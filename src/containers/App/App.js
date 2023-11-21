39e0182981462254934aafced7c8862ada66d5df
George Lykoudis <george.lykoudis@yahoo.com>
2023-11-21 10:01:03 +0200

    Clean up

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import ParticlesBg from 'particles-bg'
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo'
-import Rank from './components/Rank/Rank'
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
-import FaceRecognition from './components/FaceRecognition/FaceRecognition'
-import Signin from './components/Signin/Singin';
-import Register from './components/Register/Register';
+import Navigation from '../../components/Navigation/Navigation';
+import Logo from '../../components/Logo/Logo'
+import Rank from '../../components/Rank/Rank'
+import ImageLinkForm from '../../components/ImageLinkForm/ImageLinkForm'
+import FaceRecognition from '../../components/FaceRecognition/FaceRecognition'
+import Signin from '../../components/Signin/Singin';
+import Register from '../../components/Register/Register';
 import './App.css';
 
 const returnClarifaiRequestOptions = (imageUrl) => {
@@ -41,6 +41,21 @@ const returnClarifaiRequestOptions = (imageUrl) => {
   };
 
   return requestOptions;
+}
+
+const initialState = {
+  input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+    }
 }
 
 class App extends Component {
@@ -112,6 +127,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, { entries : count }))
           })
+          .catch(console.log);
         }
         this.displayFaceBox(this.calculateFaceLocation(response));
 
@@ -121,7 +137,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
